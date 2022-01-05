4d546c6c5860183b308b0d43f4c9c75d5ccb3dcf
Oliver Pardo <oliverius@outlook.com>
2022-01-05 11:37:28 +0000

    Tetromino data

--- games/tetris/tetris.py
+++ games/tetris/tetris.py
@@ -8,6 +8,8 @@ TETRIS
 O  L  J  S   Z   I   T
 
 https://en.wikipedia.org/wiki/Tetromino
+https://tetris.fandom.com/wiki/SRS
+https://tetris.fandom.com/wiki/Tetris_Guideline
 
 tetrominoes, name and shape (maybe colour as well)
 
@@ -53,10 +55,56 @@ class TetrominoShape(Enum):
 
 
 class Tetromino:
-   def __init__(self):
-      self.shape = TetrominoShape.NONE
-      self.orientation = 0
-      self.color = TetrominoColor.NONE
+   tetrominoes = [
+      {
+         "shape": TetrominoShape.I_SHAPE,             #
+         "color": TetrominoColor.CYAN,                #
+         "orientation": 0,                            #
+         "coordinates": [[0,0], [0,1], [0,2], [0,3]]  #  O O O O
+      },
+      {
+         "shape": TetrominoShape.J_SHAPE,             #
+         "color": TetrominoColor.BLUE,                #
+         "orientation": 0,                            #  O
+         "coordinates": [[0,0], [0,1], [0,2], [0,3]]  #  O O O O
+      },
+      {
+         "shape": TetrominoShape.L_SHAPE,             #
+         "color": TetrominoColor.ORANGE,              #
+         "orientation": 0,                            #        O
+         "coordinates": [[0,0], [1,0], [0,1], [0,2]]  #  O O O O
+      },
+      {
+         "shape": TetrominoShape.O_SHAPE,             #
+         "color": TetrominoColor.YELLOW,              #
+         "orientation": 0,                            #  O O
+         "coordinates": [[0,0], [0,1], [0,2], [0,3]]  #  O O
+      },
+      {
+         "shape": TetrominoShape.S_SHAPE,             #
+         "color": TetrominoColor.GREEN,               #
+         "orientation": 0,                            #    O O
+         "coordinates": [[0,0], [0,1], [0,2], [0,3]]  #  O O
+      },
+      {
+         "shape": TetrominoShape.T_SHAPE,             #
+         "color": TetrominoColor.PURPLE,              #
+         "orientation": 0,                            #    O
+         "coordinates": [[0,0], [0,1], [0,2], [0,3]]  #  O O O
+      },
+      {
+         "shape": TetrominoShape.Z_SHAPE,             #
+         "color": TetrominoColor.RED,                 #
+         "orientation": 0,                            #  O O
+         "coordinates": [[0,0], [0,1], [0,2], [0,3]]  #    O O
+      }
+   ]
+   def __init__(self, shape = TetrominoShape.NONE):
+      data = next((tetromino for tetromino in self.tetrominoes if tetromino["shape"] == shape), None)
+      self.shape = data["shape"]
+      self.orientation = data["orientation"]
+      self.color = data["color"]
+      self.coordinates = data["coordinates"]
 
    def rotate_left():
       pass
@@ -72,7 +120,11 @@ class Board:
       self.initialize_grid()
       
    def initialize_grid(self):
-      self.grid = [[0] * self.width for i in range(self.height)]
+      self.grid = [["◻"] * self.width for i in range(self.height)]
+
+
+
+
 
    def print_grid(self):
       printed_grid = ""
@@ -85,22 +137,30 @@ class Board:
 class TetrisGame:
    def __init__(self) -> None:
       self.board = Board(10, 20)
+      piece = self.get_next_falling_piece()
+
+
       self.board.print_grid()
 
-   def can_falling_piece_move_left():
+   def can_falling_piece_move_left(self):
       pass
 
-   def can_falling_piece_move_right():
+   def can_falling_piece_move_right(self):
       pass
 
-   def can_falling_piece_move_down():
+   def can_falling_piece_move_down(self):
       pass
 
-   def can_falling_piece_rotate_left():
+   def can_falling_piece_rotate_left(self):
       pass
 
-   def can_falling_piece_rotate_right():
+   def can_falling_piece_rotate_right(self):
       pass
+
+   def get_next_falling_piece(self) -> Tetromino:
+      falling_piece = Tetromino(TetrominoShape.L_SHAPE)
+      return falling_piece
+
 
 
 game = TetrisGame()
