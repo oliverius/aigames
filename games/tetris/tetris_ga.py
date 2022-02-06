17530f04ddb56431ee2617024b6cea1233019fbd
Oliver Pardo <oliverius@outlook.com>
2022-02-06 21:07:31 +0000

    Converted the playfield screen on its own to a frame with the playfield where we can put other controls as well

--- games/tetris/tetris_ga.py
+++ games/tetris/tetris_ga.py
@@ -1,10 +1,12 @@
 # Tetris Genetic algorithms UI
+from turtle import bgcolor
 from tetris_playable import PlayfieldScreen, config
 from tetris_agent import TetrisAgent
 from tetris_engine import TetrisEngine
 from queue import Queue
 import tkinter as tk
 import tkinter.ttk as ttk
+#import multiprocessing as mp
 
 class Window(tk.Tk):
     def __init__(self):
@@ -12,9 +14,9 @@ class Window(tk.Tk):
 
         self.title("AI Games - Tetris Genetic algorithms")
         self.geometry("1000x580")
-
-        self.playfield_screen_1 = self.get_playfield(20, 20)
-        self.playfield_screen_2 = self.get_playfield(200, 20)
+        
+        self.frame1 = PlayfieldFrame(self, width=200, height=300, bg="pink")
+        self.frame1.place(x=300,y=100)
 
         exit_button = ttk.Button(self, text="Exit", command=exit) # TODO stop everything gracefully
         exit_button.place(x=20, y=540)
@@ -43,23 +45,39 @@ class Window(tk.Tk):
         self.queue.put(data)
         print(self.queue.qsize())
 
-
     def exit(self) -> None:
         self.destroy()
 
-    def get_playfield(self, x :int, y :int) -> PlayfieldScreen:
-        playfield_screen = PlayfieldScreen(self,
+    def update_playfield(self) -> None:
+        if self.queue.empty():
+            print("it is empty")
+            self.after_cancel(self.update_playfield_timer)
+        else:
+            print("updating")
+            data = self.queue.get()           
+            self.frame1.update(data)
+            self.after(self.speed, self.update_playfield) # Call again the timer
+
+class PlayfieldFrame(tk.Frame):
+    def __init__(self, master, width :int, height :int, **kwargs):
+        self.width = width
+        self.height = height
+        super().__init__(master, width=self.width, height=self.height, **kwargs)
+
+        self.playfield_screen = PlayfieldScreen(self,
             config["playfield"]["columns"],
             config["playfield"]["rows"],
             config["playfield"]["background_color"],
             config["tetrominoes"])
-        
-        self.scale_down_playfield(playfield_screen)
-        playfield_screen.place(x=x, y=y)
-
-        return playfield_screen
-
-    def scale_down_playfield(self, playfield_screen :PlayfieldScreen) -> None:
+        self.scale_down_playfield_screen(self.playfield_screen)
+        self.playfield_screen.place(x=50, y=50)
+    
+    def scale_down_playfield_screen(self, playfield_screen :PlayfieldScreen) -> None:
+        """
+        The original playfield is bigger because it is the one used for the playable Tetris
+        So instead of copy/paste and make modifications we are using the same class
+        but scaling down after every update
+        """
         original_width = playfield_screen.width
         original_height = playfield_screen.height
         scale = 0.25 # 1/4 of the size which means that width is half and height is half
@@ -68,20 +86,13 @@ class Window(tk.Tk):
         playfield_screen.scale("all", 0, 0, scale*2, scale*2)       # It scales the contents but not the container
         playfield_screen.config(width=new_width, height=new_height) # It scales the container
 
-    def update_playfield(self) -> None:
-        if self.queue.empty():
-            print("it is empty")
-            self.after_cancel(self.update_playfield_timer)
-        else:
-            print("updating")
-            data = self.queue.get()
-            self.playfield_screen_1.draw(
-                data["rows_from_the_bottom_up"],
-                data["falling_piece_shape"],
-                data["falling_piece_coordinates"],
-                data["ghost_dropped_piece_coordinates"])
-            self.scale_down_playfield(self.playfield_screen_1)
-            self.after(self.speed, self.update_playfield) # Call again the timer
+    def update(self, data :dict) -> None:
+        self.playfield_screen.draw(
+            data["rows_from_the_bottom_up"],
+            data["falling_piece_shape"],
+            data["falling_piece_coordinates"],
+            data["ghost_dropped_piece_coordinates"])
+        self.scale_down_playfield_screen(self.playfield_screen)
 
 if __name__ == "__main__":
     window = Window()
